bdf12a3b9af44a151fb21763fe0879b81bf5a3b7
Humaira <144988823+Moiralah@users.noreply.github.com>
2025-05-25 00:49:21 +0800

    Add cpp files

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: huidris <huidris@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/24 06:28:46 by huidris           #+#    #+#             */
+/*   Updated: 2025/05/24 09:40:30 by huidris          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+int	main(int ac, char **argv)
+{
+	if(ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	if (ac > 1)
+	{
+		int i = 0;
+		while (argv[++i])
+		{
+			int j = -1;
+			while (argv[i][++j])
+				std::cout << (char)toupper(argv[i][j]);
+		}
+	}
+	std :: cout << '\n';
+	return 0;
+}
